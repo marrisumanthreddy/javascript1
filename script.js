1a4be6da609a898b3b87a5dccbf9100aa72b290b
marrisumanthreddy <marrisumanthreddy@gmail.com>
2026-01-19 19:52:06 +0530

    login

---
+++ script.js
@@ -0,0 +1,74 @@
+//let username=prompt("Enter your username");
+//let password=prompt("Enter your password");
+/* if(username=="sumanth"){
+      if(password=="123"){
+        alert("welcome to the instagram")
+      } 
+      else{
+        alert("password is incorrect");
+      } 
+ }else{
+    alert("username is incorrect");
+ }
+*/
+/*if(username=="sumanth" && password=="123"){
+    console.log("welcome to the instagram");
+}else{
+    console.log("Invalid Credentials");
+    alert("Invalid");
+}*/
+/*console.log(true&&true);
+console.log(true&&false);
+console.log(false&&true);
+console.log(false&&false);
+if(100 && 80){
+    console.log("good")
+}*/
+/*let math=27;
+let social=32;
+let science=23;
+let telugu=4;
+let english=29;
+if(math>=35||social>=35||science>=35||telugu>=35||english>=35)
+  {
+  console.log("congrats,you have passed the exam")
+  alert("you have passed the exam")
+}
+else{
+  console.log("you have failed all exams");
+  alert("you are failed")
+}*/
+/*let isPassed = true;
+console.log("isPassed",isPassed);
+console.log("isPassed",!isPassed);
+*/
+let sureshAge,hareeshAge, nareshAge;
+let currentTotalAge=0;
+sureshAge=+prompt("suresh,what is your age");
+thankYouMessage("suresh",sureshAge);
+
+hareeshAge=+prompt("hareesh, what is your age");
+thankYouMessage("hareesh",hareeshAge);
+
+nareshAge=+prompt("naresh,what is your age");
+thankYouMessage("naresh",nareshAge);
+
+function thankYouMessage(name,age){
+  console.log(`${name} age is ${age}`);
+  if (age>18){
+    console.log(`${name},You Will Get Driving License`);
+  }
+  else{
+    console.log(`${name},You Need to Wait ${18-age} More Years to Get Driving License`);
+    
+  }
+  currentTotalAge=currentTotalAge+age;
+  console.log(currentTotalAge);
+  console.log("Thank you for answering");
+  if(age>18){
+  console.log("Please Subscribe To Our Channel");
+  }
+  else{
+   console.log("error")
+  }
+}
